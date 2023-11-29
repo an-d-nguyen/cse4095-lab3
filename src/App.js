a35ba4329c55752a834bbc842f7ad17a845b62ae
An Nguyen <anan.0192@gmail.com>
2023-11-28 22:46:04 -0500

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const jokes = [
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "I'm reading a book on anti-gravity. It's impossible to put down!",
+    "Did you hear about the mathematician who’s afraid of negative numbers? He'll stop at nothing to avoid them!"
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jokes</h1>
+        <ul>
+          {jokes.map(joke => <li key={joke}>{joke}</li>)}
+        </ul>
       </header>
     </div>
   );
